8ad3936ba88f2c5fc04e712ea4bd38fb4425d0aa
nayrneerg <ryan@thisiscontroltower.com>
2026-03-13 23:02:06 -0700

    Update and rename next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['localhost'],
   },
@@ -9,6 +8,6 @@ const nextConfig: NextConfig = {
       allowedOrigins: ['localhost:3000'],
     },
   },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
